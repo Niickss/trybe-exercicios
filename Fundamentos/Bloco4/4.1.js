66f02c4f4fcd17b05e9cbfc3c063d269e6bc9519
Niickss <nicoleg.cze@gmail.com>
2021-11-26 22:47:09 -0300

    exercicio parte 3

--- Fundamentos/Bloco4/4.1.js
+++ Fundamentos/Bloco4/4.1.js
@@ -1,11 +1,16 @@
-const nota = 100;
+const currentHour = 24;
+let message = "";
 
-if (nota >= 80) {
-    console.log("Parabéns, você foi aprovada(o)!");
+if (currentHour >= 22) {
+  console.log ("Não deveríamos comer nada, é hora de dormir!");
 }
-    else if (nota < 80 && nota >= 60) {
-    console.log("Você está na nossa lista de espera!");
-    }
-        else (nota < 60) {
-        console.log("Você foi reprovada(o)!");
-        }
+  if else (currentHour =< 22) {
+    console.log ("Rango da noite, vamos jantar :D");
+  } if else (currentHour >=14 && currentHour < 18) {
+    console.log ("Vamos fazer um bolo pro café da tarde?");
+  }   if else (currentHour === 11 && currentHour <14) {
+    console.log ("Hora do almoço!!!");
+  }     else (currentHour === 4 && currentHour <11) {
+    console.log ("Hmmm, cheiro de café recém passado!");
+  }
+console.log message
