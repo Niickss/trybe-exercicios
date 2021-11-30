b4231b863beb8cef075fa7c2f59bd151957eb226
Niickss <nicoleg.cze@gmail.com>
2021-11-29 22:54:36 -0300

    3 exercicio

--- Fundamentos/Bloco4/4.1.js
+++ Fundamentos/Bloco4/4.1.js
@@ -1,8 +1,11 @@
 const a = 64;
 const b = 30;
+const c = 10;
 
-if (a>=b) {
-    console.log (a);
-} else if (a<=b) {
+if (c > b && a) {
+    console.log (c);
+} else if (b < a && c) {
     console.log (b);
+} else {
+    console.log (a)
 }
