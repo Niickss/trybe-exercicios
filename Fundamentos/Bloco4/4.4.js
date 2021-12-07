4bec4eee8bb7243753caca584e752b5bb97b0120
Niickss <nicoleg.cze@gmail.com>
2021-12-07 09:38:31 -0300

    parte 2 exercicio 1

--- Fundamentos/Bloco4/4.4.js
+++ Fundamentos/Bloco4/4.4.js
@@ -1,21 +1,9 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-  };
-
-  let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim'
-  }
-
- for (chaves in info,info2) {
-     if (info[chaves] != info2[chaves])  {
-     console.log (info[chaves] + " e " + info2[chaves]);
+function verificaPalindrome(string) {
+ for (let i = 0; i < string.length / 2; i++) 
+    if (string[i] !=string[string.length - i - 1]) {
+    return false;
     } else {
-     console.log ("Ambos recorrentes");
-    }
- };
+        return true;
+    } 
+} console.log (verificaPalindrome('arara'));
+  console.log (verificaPalindrome('desenvolvimento'));
